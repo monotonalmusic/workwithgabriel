6d390a85a31fd5f1929d6bc688309b59069bb0ee
Gabriel <gehughes28@gmail.com>
2025-03-03 09:28:49 +0100

    fixed small issues

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.getElementById("burger").addEventListener("click", function () {
   document.getElementById("menu").classList.add("active");
+  document.getElementById("bottom-border-header").classList.add("hide");
 });
 
 document.getElementById("close").addEventListener("click", function () {
   document.getElementById("menu").classList.remove("active");
+  document.getElementById("bottom-border-header").classList.remove("hide");
 });
 
 // Animate bottom border from right to left
